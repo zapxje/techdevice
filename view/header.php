9fc67ba42602ab973e353d4caeb2a2405ae4b4b3
dangkhoa0807 <khoatran2b@gmail.com>
2023-11-05 19:14:50 +0700

    cart

--- view/header.php
+++ view/header.php
@@ -15,7 +15,7 @@
 
     <!-- Bootstrap -->
     <link type="text/css" rel="stylesheet" href="view/assets/css/bootstrap.min.css" />
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <!-- Slick -->
     <link type="text/css" rel="stylesheet" href="view/assets/css/slick.css" />
     <link type="text/css" rel="stylesheet" href="view/assets/css/slick-theme.css" />
