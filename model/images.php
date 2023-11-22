b1eeab29a29d6650513db1e0847d3ad372d6a7e1
zapxje <minhgiap1357@gmail.com>
2023-11-22 14:23:30 +0700

    complete add product

--- model/images.php
+++ model/images.php
@@ -4,3 +4,10 @@ function getImageByProduct($id)
     $sql = "SELECT * FROM images_product WHERE id_product = " . $id;
     return getAll($sql);
 }
+// function addProduct($idCategory, $idBrand, $name, $price, $price_sale, $quantity, $description, $image)
+// {
+//     $sql = "INSERT INTO products(id_category, id_brand, name, price, price_sale, quantity, description, image) 
+//     VALUES(" . $idCategory . ", " . $idBrand . " , '" . $name . "'," . $price . ", " . $price_sale . ", " . $quantity . ", '" . $description . "', '" . $image . "')";
+//     echo $sql;
+//     return querySql($sql);
+// }
