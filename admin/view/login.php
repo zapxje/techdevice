1846522744ea6ba3e01405e63815e287864431ea
zapxje <minhgiap1357@gmail.com>
2023-11-24 19:08:29 +0700

    login admin

--- admin/view/login.php
+++ admin/view/login.php
@@ -3,34 +3,34 @@
         <div class="login-content">
             <div class="login-logo">
                 <a href="index.html">
-                    <img class="align-content" src="images/logo.png" alt="">
+                    <img width="350px" class="align-content" src="assets/images/logo/logo_x.png" alt="">
                 </a>
             </div>
             <div class="login-form">
                 <form>
                     <div class="form-group">
-                        <label>Email address</label>
-                        <input type="email" class="form-control" placeholder="Email">
+                        <label>Tên đăng nhập</label>
+                        <input type="text" name="name" class="form-control" placeholder="Tên đăng nhập">
                     </div>
                     <div class="form-group">
-                        <label>Password</label>
-                        <input type="password" class="form-control" placeholder="Password">
+                        <label>Mặt khẩu</label>
+                        <input type="password" name="password" class="form-control" placeholder="Mật khẩu">
                     </div>
                     <div class="checkbox">
                         <label>
-                            <input type="checkbox"> Remember Me
+                            <input type="checkbox"> Nhớ mặt khẩu
                         </label>
                         <label class="pull-right">
-                            <a href="#">Forgotten Password?</a>
+                            <a href="#">Quên mật khẩu?</a>
                         </label>
 
                     </div>
                     <button type="submit" class="btn btn-success btn-flat m-b-30 m-t-30">Sign in</button>
                     <div class="social-login-content">
-                        <div class="social-button">
+                        <!-- <div class="social-button">
                             <button type="button" class="btn social facebook btn-flat btn-addon mb-3"><i class="ti-facebook"></i>Sign in with facebook</button>
                             <button type="button" class="btn social twitter btn-flat btn-addon mt-2"><i class="ti-twitter"></i>Sign in with twitter</button>
-                        </div>
+                        </div> -->
                     </div>
                     <div class="register-link m-t-15 text-center">
                         <p>Don't have account ? <a href="#"> Sign Up Here</a></p>
